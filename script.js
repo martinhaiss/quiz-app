1de5c7f2cbd537f2f24aee029df358b2b74f45d2
Martin Haiss <haissmartin@gmail.com>
2021-09-01 21:46:17 +0200

    add classes, buttons and script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+
+- mehrere main Elemente einfügen
+- class main--hidden erzeugen
+- Navigation <a> in <buttons> umwandeln
+
+- Zugriff auf Elemente verschaffen
+  -> 8 Variablen ( 4 x main, 4 x button)
+
+- 4 eventListener erzeugen
+   - bei einem main Element die Klasse main-hidden entfernen
+   - bei 3 main Elementen die Klasse main--hidden hinzufügen
+
+   /*
