b2b5cfa71dc5bbd245ffe4222ecdea9fe19487f7
Martin Haiss <haissmartin@gmail.com>
2021-09-01 23:08:21 +0200

    improved id-names in index.html, corrected main.css and created variables and eventListener

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-/*
+/* 
+   
+   Navigation-Start 
+   
+   */
 
-- mehrere main Elemente einfügen
-- class main--hidden erzeugen
-- Navigation <a> in <buttons> umwandeln
+const mainHome = document.querySelector('#main__home')
+const mainBookmarks = document.querySelector('#main__bookmarks')
+const mainCreate = document.querySelector('#main__create')
+const mainProfile = document.querySelector('#main__profile')
 
-- Zugriff auf Elemente verschaffen
-  -> 8 Variablen ( 4 x main, 4 x button)
+const buttonHome = document.querySelector('#footer__home')
+const buttonBookmarks = document.querySelector('#footer__bookmarks')
+const buttonCreate = document.querySelector('#footer__create')
+const buttonProfile = document.querySelector('#footer__profile')
 
-- 4 eventListener erzeugen
-   - bei einem main Element die Klasse main-hidden entfernen
-   - bei 3 main Elementen die Klasse main--hidden hinzufügen
+buttonHome.addEventListener('click', () => {
+  mainHome.classList.add('main--show')
+  mainBookmarks.classList.remove('main--show')
+  mainCreate.classList.remove('main--show')
+  mainProfile.classList.remove('main--show')
 
-   /*
+  buttonHome.classList.add('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonBookmarks.addEventListener('click', () => {
+  mainHome.classList.remove('main--show')
+  mainBookmarks.classList.add('main--show')
+  mainCreate.classList.remove('main--show')
+  mainProfile.classList.remove('main--show')
+
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.add('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonCreate.addEventListener('click', () => {
+  mainHome.classList.remove('main--show')
+  mainBookmarks.classList.remove('main--show')
+  mainCreate.classList.add('main--show')
+  mainProfile.classList.remove('main--show')
+
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.add('active')
+  buttonProfile.classList.remove('active')
+})
+
+buttonProfile.addEventListener('click', () => {
+  mainHome.classList.remove('main--show')
+  mainBookmarks.classList.remove('main--show')
+  mainCreate.classList.remove('main--show')
+  mainProfile.classList.add('main--show')
+
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.add('active')
+})
+
+/* 
+   
+   Navigation-Ende 
+   
+   */
